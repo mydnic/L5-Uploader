f6fa9d2a26c8bbd9728245fda035db381e843e9e
Clement <rigoclement@mydnic.be>
2015-05-21 14:48:19 +0200

    possibility to specify path and return fullpath

--- src/Mydnic/Uploader/Uploader.php
+++ src/Mydnic/Uploader/Uploader.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Auth;
 
 class Uploader {
 
-    public static function upload($file)
+    public static function upload($file, $returnFullPath = false, $folder = null)
     {
-        $destinationPath = public_path().'/uploads/';
+        if ($folder == null) {
+            $folder = 'uploads';
+        }
+
+        $destinationPath = public_path().'/'.$folder.'/';
 
         if (Auth::check()) {
             $filename = str_replace(' ', '_', str_random(20) . Auth::id() . $file->getClientOriginalName());
@@ -16,8 +20,11 @@ class Uploader {
         }
 
         $uploadSuccess = $file->move($destinationPath, $filename);
+
+        if ($returnFullPath) {
+            return '/' . $folder . '/' . $filename;
+        }
         
         return $filename;
     }
-
 }
